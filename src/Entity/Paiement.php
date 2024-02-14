a877d11c6d8fb1a96ed7d1dce23c96db41834921
nour <nour.qahman@esen.tn>
2024-02-14 21:12:06 +0100

    Pi

---
+++ src/Entity/Paiement.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaiementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaiementRepository::class)]
+class Paiement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $numt = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dated = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumt(): ?int
+    {
+        return $this->numt;
+    }
+
+    public function setNumt(int $numt): static
+    {
+        $this->numt = $numt;
+
+        return $this;
+    }
+
+    public function getDated(): ?\DateTimeInterface
+    {
+        return $this->dated;
+    }
+
+    public function setDated(\DateTimeInterface $dated): static
+    {
+        $this->dated = $dated;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
