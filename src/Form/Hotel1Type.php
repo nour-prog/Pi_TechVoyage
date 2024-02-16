24f683594229a1bc8ccbe49e0441d20dc5d815b8
malekbensdira <100678862+malekbensdira@users.noreply.github.com>
2024-02-17 00:36:43 +0100

    crud front final sans controlle

---
+++ src/Form/Hotel1Type.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Hotel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Hotel1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('nbretoile')
+            ->add('emplacement')
+            ->add('avis')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Hotel::class,
+        ]);
+    }
+}
