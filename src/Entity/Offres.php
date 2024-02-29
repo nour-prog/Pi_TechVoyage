d858536ebdcb851e9b06d135934af1b3fc026db2
chaimaattyaoui <153545077+chaimaattyaoui@users.noreply.github.com>
2024-02-29 17:14:49 +0100

    try5

--- src/Entity/Offres.php
+++ src/Entity/Offres.php
@@ -6,7 +6,7 @@ use App\Repository\OffresRepository;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\UX\Turbo\Attribute\Broadcast;
+
 
 #[ORM\Entity(repositoryClass: OffresRepository::class)]
 #[Broadcast]
